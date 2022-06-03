ac1256a9006258fd580837eaaeb4cba337de0cc0
Yash_Shhhhh <66167011+yash1sh@users.noreply.github.com>
2022-06-03 19:21:10 +0530

    Delete Cards directory

--- Cards/index.js
+++
@@ -1,9 +0,0 @@
-const pic = document.querySelectorAll(".pic");
-
-pic.forEach((picc) => {
-  picc.addEventListener("click", () => {
-    let v1 = document.querySelector(".active");
-    v1.classList.remove("active");
-    picc.classList.add("active");
-  });
-});
